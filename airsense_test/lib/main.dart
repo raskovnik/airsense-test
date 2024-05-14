4e3c7335c84cbe99f54b03f7e5f8ca7efbf2ff08
raskovnik <4lch3my.3ck0@gmail.com>
2024-05-14 23:45:49 +0300

    add refresh indicator to refresh data

--- airsense_test/lib/main.dart
+++ airsense_test/lib/main.dart
@@ -1,22 +1,27 @@
+import 'package:airsense_test/blocs/data_cubit.dart';
 import 'package:airsense_test/ui/homepage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Airsense Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return BlocProvider(
+      create: (context) => DataCubit()..fetchData(), // Provide DataCubit here
+      child: MaterialApp(
+        title: 'Airsense Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: const HomePage(),
       ),
-      home: const HomePage(),
     );
   }
 }
--- airsense_test/lib/ui/homepage.dart
+++ airsense_test/lib/ui/homepage.dart
@@ -15,50 +15,61 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final currentDate = DateTime.now();
-    final dateTo = DateFormat('yyyy-MM-dd HH:mm:ss').format(currentDate);
-    final dateFrom = DateFormat('yyyy-MM-dd HH:mm:ss').format(currentDate.subtract(Duration(days: 7)));
+
+    Future<void> _refreshData() async {
+      context.read<DataCubit>().fetchData();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text('Test App')),
       ),
-      body: BlocProvider(
-        create: (context) => DataCubit()..fetchData(),
-        child: BlocBuilder<DataCubit, DataState>(
-          builder: (context, state) {
-            if (state is DataLoading) {
-              return const Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Center(child: CircularProgressIndicator()),
-                  Center(child: Text("Loading data for device 00000000000000bb"))
-                ],
-              );
-            } else if (state is DataLoaded) {
-              final temperatureGroupList = [
-                TimeGroup(id: '1', data: state.data)
-              ];
-              return Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: Column(
+      body: RefreshIndicator(
+        onRefresh: () async {
+          BlocProvider.of<DataCubit>(context).fetchData();
+        },
+        child: SingleChildScrollView(
+          physics: const AlwaysScrollableScrollPhysics(),
+          child: BlocBuilder<DataCubit, DataState>(
+            builder: (context, state) {
+              if (state is DataLoading) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    Center(child: Text("Temperature for date $dateFrom - $dateTo")),
-                    AspectRatio(
-                      aspectRatio: 16/9,
-                      child: DChartLineT(groupList: temperatureGroupList,
-                    )
-                    ),
+                    SizedBox(height: MediaQuery.of(context).size.height * 0.4),
+                    Center(child: CircularProgressIndicator()),
+                    Center(child: Text("Loading data for device 00000000000000bb"))
                   ],
-                ),
-              );
-            } else if (state is DataError) {
-              return const Center(child: Text("Error loading data"));
-            } else {
-              return Container();
+                );
+              } else if (state is DataLoaded) {
+                final currentDate = DateTime.now();
+                final dateTo = DateFormat('yyyy-MM-dd HH:mm:ss').format(currentDate);
+                final dateFrom = DateFormat('yyyy-MM-dd HH:mm:ss').format(currentDate.subtract(Duration(days: 7)));
+                final temperatureGroupList = [
+                  TimeGroup(id: '1', data: state.data)
+                ];
+                return Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Column(
+                    children: [
+                      Center(child: Text("Temperature for date $dateFrom - $dateTo")),
+                      AspectRatio(
+                        aspectRatio: 16/9,
+                        child: DChartLineT(groupList: temperatureGroupList,
+                      )
+                      ),
+                    ],
+                  ),
+                );
+              } else if (state is DataError) {
+                return const Center(child: Text("Error loading data"));
+              } else {
+                return Container();
+              }
             }
-          }
-        )
+          ),
+        ),
       )
     );
   }
